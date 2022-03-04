68375a042e7c22ade40d076f286fc2a953a565ff
King David <davidwiers@davids-mbp.lan>
2022-03-04 13:16:16 -0600

    adding files after successfully launching the website to display Hello World

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+#importing the tools
+#we'll use Flask to render a template, redirecting to another url, and creating a url
 from flask import Flask, render_template, redirect, url_for
+#we'll use PyMongo to interact with our Mongo database
 from flask_pymongo import PyMongo
+#to use the scraping code, we will convert from Jupyter to Python
 import scraping
 
 app = Flask(__name__)
